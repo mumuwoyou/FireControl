16c43a4fbe9ed5cdf491611a2443eea085e0b838
linlin <mumuwoyou@msn.com>
2019-12-13 21:50:54 +0800

    添加项目文件。

---
+++ FireControl/FireControlForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FireControl
+{
+    partial class FireControlForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(2, 61);
+            this.webBrowser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(22, 24);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1207, 696);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(356, 13);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 30);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(122, 14);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(184, 28);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "开始日期：";
+            // 
+            // FireControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 748);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.webBrowser);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FireControlForm";
+            this.Text = "防火日志填单工具";
+            this.Load += new System.EventHandler(this.FireControlForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FireControl/FireControlForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FireControl
+{
+    public partial class FireControlForm : Form
+    {
+        DateTime current;
+        string names = "林琳,史丽娟,沈富娟,索亚芳";
+        string[] nameArray;
+        int index = 0;
+        int index1 = 3;
+        public FireControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void FireControlForm_Load(object sender, EventArgs e)
+        {
+            this.webBrowser.Url = new Uri("http://223.4.70.2:85/FrameSet/Login.aspx");
+            nameArray = names.Split(',');
+            current = this.dateTimePicker1.Value;
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (this.webBrowser.Url == new Uri("http://223.4.70.2:85/FrameSet/Login.aspx"))
+            {
+                MessageBox.Show("请先登入！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (this.webBrowser.Url != new Uri("http://223.4.70.2:85/JCDAPage/XFGZJLPage/FHXC_SimpleAddPage.aspx"))
+            {
+                this.webBrowser.Url = new Uri("http://223.4.70.2:85/JCDAPage/XFGZJLPage/FHXC_SimpleAddPage.aspx");
+                this.btnStart.Text = current.Date.ToString("yyyyMMdd");
+                
+            }
+            else
+            {
+                
+                HtmlElement tbDate = this.webBrowser.Document.All["ctl00_MainContent_txtRQ"];
+                HtmlElement tbNumber = this.webBrowser.Document.All["ctl00_MainContent_txtBH"];
+                HtmlElement tbName = this.webBrowser.Document.All["ctl00_MainContent_txtXCY"];
+                HtmlElement tbCall = this.webBrowser.Document.All["ctl00_MainContent_txtXCCS"];
+                HtmlElement tbContent = this.webBrowser.Document.All["ctl00_MainContent_txtFXWT"];
+                HtmlElement tbChecker = this.webBrowser.Document.All["ctl00_MainContent_txtJCR"];
+                HtmlElement btnAdd = this.webBrowser.Document.All["ctl00_MainContent_btnAdd"];
+
+                //this.btnStart.Enabled = false;
+                String c = current.Date.Year.ToString() + "年"
+                         + current.Date.Month.ToString() + "月"
+                         + current.Date.Day.ToString() + "日";
+                tbDate.SetAttribute("Value", c);
+                tbNumber.SetAttribute("Value", current.ToString("yyyyMMdd"));
+                tbName.SetAttribute("Value", nameArray[index%4]+","+ nameArray[index1%4]);
+                tbCall.SetAttribute("Value", "2");
+                tbContent.SetAttribute("Value", "正常");
+                tbChecker.SetAttribute("Value", "尹小龙");
+                index += 1;
+                index1 += 1;
+                current = current.Date.AddDays(-1);
+                this.btnStart.Text = current.ToString("yyyyMMdd");
+                //this.btnStart.Enabled = true;
+                btnAdd.InvokeMember("click");
+               
+                //this.webBrowser.Url = new Uri("http://223.4.70.2:85/JCDAPage/XFGZJLPage/FHXC_SimpleAddPage.aspx");
+            }
+                
+                         
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            current = this.dateTimePicker1.Value;
+            this.btnStart.Text = this.dateTimePicker1.Value.ToString("yyyyMMdd");
+        }
+    }
+}
